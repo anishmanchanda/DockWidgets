360aa60b235929fed328d3627597926cb6de801c
Anish Manchanda <anishmanchanda2006@gmail.com>
2025-07-10 15:02:28 +0530

    Initial Commit

---
+++ DockWidgets/DockWidgetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DockWidgetsApp.swift
+//  DockWidgets
+//
+//  Created by Anish Manchanda on 7/10/25.
+//
+
+import SwiftUI
+
+@main
+struct DockWidgetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
